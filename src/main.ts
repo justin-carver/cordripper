09a25eea2a8d9bf238bbd4b036ebb372c8d2133b
Justin Carver <justin.carver@hotmail.com>
2023-04-17 21:39:16 -0500

    Successfully able to download data from user.

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,97 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import {
+  Client,
+  TextChannel,
+  ChannelType,
+  GatewayIntentBits,
+} from 'discord.js';
+import fs from 'fs';
+import * as dotenv from 'dotenv';
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+dotenv.config();
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+const fetchAllMessagesByUser = async (
+  client: Client,
+  guildId: string,
+  userId: string,
+): Promise<unknown[]> => {
+  const userMessages = [];
+  const guild = client.guilds.cache.get(guildId);
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+  if (!guild) {
+    throw new Error('Guild not found');
+  }
+
+  const channels = Array.from(
+    guild.channels.cache
+      .filter((channel: TextChannel) => channel.type === ChannelType.GuildText)
+      .values(),
+  ) as TextChannel[];
+
+  for (const channel of channels) {
+    console.log(`Currently in #${channel.name}...`);
+    try {
+      let fetchedMessages = await channel.messages.fetch({ limit: 100 });
+      console.log(fetchedMessages[0]);
+      let userChannelMessages = fetchedMessages.filter(
+        (message) => message.author.id === userId,
+      );
+
+      while (fetchedMessages.size > 0) {
+        userMessages.push(...userChannelMessages);
+        const lastMessageId = fetchedMessages.last()?.id;
+        fetchedMessages = await channel.messages.fetch({
+          limit: 100,
+          before: lastMessageId,
+        });
+
+        userChannelMessages = fetchedMessages.filter(
+          (message) => message.author.id === userId,
+        );
+      }
+    } catch (error) {
+      console.error(
+        `Error fetching messages from channel: ${channel.id}`,
+        error,
+      );
+    }
+  }
+
+  return userMessages;
+};
+
+const saveMessagesToFile = (messages: unknown): void => {
+  const data = JSON.stringify(messages);
+  fs.writeFileSync('user_messages.json', data);
+};
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildIntegrations,
+  ],
+});
+
+client.once('ready', async () => {
+  console.log('Bot is ready!');
+  try {
+    console.log('Beginning to fetch messages...');
+    const userMessages = await fetchAllMessagesByUser(
+      client,
+      process.env.GUILDID,
+      process.env.TARGET_USER,
+    );
+    saveMessagesToFile(userMessages);
+    console.log(
+      `A total of ${userMessages.length} messages from ${process.env.TARGET_USER} have been saved to a file.`,
+    );
+  } catch (error) {
+    console.error('Error fetching all messages by user:', error);
+    console.log('Error fetching messages. Please try again later.');
+  }
+});
+
+client.login(process.env.TOKEN);
